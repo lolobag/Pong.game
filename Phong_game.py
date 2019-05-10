ab544d9c63999ee03123ef9e701d353c060fa87e
lolobag <49460087+lolobag@users.noreply.github.com>
2019-05-10 16:00:45 +0200

    Update Phong_game.py
    Fixed gamer mistakes.
    Added ball coalitions with paddles.

--- Phong_game.py
+++ Phong_game.py
@@ -11,26 +11,26 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Paddle A
-puddle_a = turtle.Turtle()
-puddle_a.speed(0)
-puddle_a.shape("square")
-puddle_a.color("white")
-puddle_a.shapesize(stretch_wid=5, stretch_len=1)
-puddle_a.penup()
-puddle_a.goto(-350, 0)
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 # Paddle B
-puddle_b = turtle.Turtle()
-puddle_b.speed(0)
-puddle_b.shape("square")
-puddle_b.color("white")
-puddle_b.shapesize(stretch_wid=5, stretch_len=1)
-puddle_b.penup()
-puddle_b.goto(+350, 0)
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(+350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0)
+ball.speed ()
 ball.shape("square")
 ball.color("white")
 ball.shapesize(stretch_wid=1, stretch_len=1)
@@ -41,32 +41,32 @@ ball.dy = -1
 
 
 # Function
-def puddle_a_up():
-    y = puddle_a.ycor()
+def paddle_a_up():
+    y = paddle_a.ycor()
     y += 20
-    puddle_a.sety(y)
+    paddle_a.sety(y)
 
-def puddle_a_down():
-    y = puddle_a.ycor()
+def paddle_a_down():
+    y = paddle_a.ycor()
     y -= 20
-    puddle_a.sety(y)
+    paddle_a.sety(y)
 
-def puddle_b_up():
-    y = puddle_b.ycor()
+def paddle_b_up():
+    y = paddle_b.ycor()
     y += 20
-    puddle_b.sety(y)
+    paddle_b.sety(y)
 
-def puddle_b_down():
-    y = puddle_b.ycor()
+def paddle_b_down():
+    y = paddle_b.ycor()
     y -= 20
-    puddle_b.sety(y)
+    paddle_b.sety(y)
 
 # Keyboard binding
 wn.listen()
-wn.onkeypress(puddle_a_up, "w")
-wn.onkeypress(puddle_a_down, "e")
-wn.onkeypress(puddle_b_up, "Up")
-wn.onkeypress(puddle_b_down, "Down")
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "e")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 
 
@@ -76,6 +76,7 @@ while True:
     wn.update()
 
     # Move the ball
+    ball.speed = 3
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
@@ -94,5 +95,14 @@ while True:
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddel and ball coallision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() +40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() +40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
         ball.dx *= -1
 
